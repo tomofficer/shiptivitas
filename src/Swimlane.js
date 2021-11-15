1502cde32e0f7091e2490e0791e9b5e6c50971e5
Tom Officer <thomasofficerjr@gmail.com>
2021-11-14 21:00:51 -0800

    initial commit

---
+++ src/Swimlane.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Card from './Card';
+import './Swimlane.css';
+
+export default class Swimlane extends React.Component {
+  render() {
+    const cards = this.props.clients.map(client => {
+      return (
+        <Card
+          key={client.id}
+          id={client.id}
+          name={client.name}
+          description={client.description}
+          status={client.status}
+        />
+      );
+    })
+
+    //check for dragula here too line 24?
+    return (
+      <div className="Swimlane-column">
+        <div className="Swimlane-title">{this.props.name}</div>
+        <div className="Swimlane-dragColumn" ref={this.props.dragulaRef}>
+          {cards}
+        </div>
+      </div>);
+  }
+
+}
